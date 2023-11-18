7d0d3bc43855154c355a361bf9f08d26c2488901
asgarovf <fasgarov2002@gmail.com>
2023-11-18 17:02:03 +0300

    [nouns-avatars] feat: seed generation

--- clave-nouns-avatars/src/NounsAvatar.tsx
+++ clave-nouns-avatars/src/NounsAvatar.tsx
@@ -1,28 +1,40 @@
 import { $MixedElement } from "./types";
-import { nounsContract } from "./utils";
-import { BigNumber } from "ethers";
-import { useEffect, useState } from "react";
+import { formatPx, getSeedForNounPart, nounsContract } from "./utils";
+import { ComponentPropsWithoutRef, useEffect, useMemo, useState } from "react";
 
-type Props = {
-  size: number;
+interface Props
+  extends Omit<ComponentPropsWithoutRef<"div">, "dangerouslySetInnerHTML"> {
+  size?: number;
   address: string;
-};
+}
 
-export const NounsAvatar = ({ size, address }: Props): $MixedElement => {
+export const NounsAvatar = ({
+  size = 32,
+  address,
+  style,
+  ...rest
+}: Props): $MixedElement => {
   const [svg, setSvg] = useState<string | null>(null);
 
+  const seed = useMemo(() => {
+    const parsedAddress = address.startsWith("0x") ? address.slice(2) : address;
+    return getSeedForNounPart(parsedAddress);
+  }, [address]);
+
   useEffect(() => {
-    nounsContract
-      .generateSVGImage([1, 10, 3, 9, 1])
-      .then((res: string): void => {
-        setSvg(atob(res));
-      });
-  }, []);
+    if (seed == null) {
+      return;
+    }
+    nounsContract.generateSVGImage(seed).then((res: string): void => {
+      setSvg(atob(res));
+    });
+  }, [seed]);
 
   return (
     <div
-      style={{ width: "120px", height: "120px" }}
+      style={{ width: formatPx(size), height: formatPx(size), ...style }}
       dangerouslySetInnerHTML={{ __html: svg }}
-    ></div>
+      {...rest}
+    />
   );
 };
--- clave-nouns-avatars/src/utils.ts
+++ clave-nouns-avatars/src/utils.ts
@@ -7,7 +7,6 @@ const provider = new ethers.providers.JsonRpcProvider(
 
 const addresses = {
   nounsDescriptor: "0x0Cfdb3Ba1694c2bb2CFACB0339ad7b1Ae5932B63",
-  nounsSeeder: "0xCC8a0FB5ab3C7132c1b2A0109142Fb112c4Ce515",
 };
 
 export const nounsContract = new Contract(
@@ -15,3 +14,57 @@ export const nounsContract = new Contract(
   NounsDescriptorAbi,
   provider
 );
+
+export const counts = {
+  glasses: 21,
+  head: 234,
+  body: 30,
+  accessory: 137,
+  background: 2,
+};
+
+/**
+ *
+ * @param string - The string to be hashed
+ * @returns A number that represents the hash of the string
+ */
+export const hashString = (string: string): number => {
+  if (typeof string != "string") {
+    return 1;
+  }
+  let id = 0;
+  for (let i = 0; i < string.length; i++) {
+    id += string.charCodeAt(i);
+  }
+  return id;
+};
+
+/**
+ *
+ * @param parsedAddress - The 20 byte address of the account
+ * @returns An array of 5 numbers that represent the seed for each noun part
+ */
+export const getSeedForNounPart = (parsedAddress: string): Array<number> => {
+  const first = parsedAddress.slice(0, 8);
+  const second = parsedAddress.slice(8, 16);
+  const third = parsedAddress.slice(16, 24);
+  const fourth = parsedAddress.slice(24, 32);
+  const fifth = parsedAddress.slice(32, 40);
+
+  const background = hashString(first) % counts.background;
+  const body = hashString(second) % counts.body;
+  const accessory = hashString(third) % counts.accessory;
+  const head = hashString(fourth) % counts.head;
+  const glasses = hashString(fifth) % counts.glasses;
+
+  return [background, body, accessory, head, glasses];
+};
+
+/**
+ *
+ * @param px - The number of pixels to be formatted
+ * @returns A string that represents the number of pixels
+ */
+export const formatPx = (px: number): string => {
+  return `${px}px`;
+};
